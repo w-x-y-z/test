52a38377b7fb73fae636bb7054f21cf65ea16de3
w-x-y-z <146482222+w-x-y-z@users.noreply.github.com>
2024-05-06 18:53:38 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,33 @@
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Instalado');
+let d
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Instalado");
 });
 
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push recibido.');
+self.addEventListener("push", function (event) {
+  console.log("[Service Worker] Push recibido.");
 
-  const title = 'Notificación Push';
+  const title = "Notificación Push";
   const options = {
-    body: 'Contenido de la notificación push.',
-    icon: 'icons/icon-192x192.png',
-    badge: 'icons/badge.png'
+    body: "Contenido de la notificación push.",
+    icon: "icons/icon-192x192.png",
+    badge: "icons/badge.png",
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
 });
+
+function simularNotificacionPush() {
+  navigator.serviceWorker.ready.then(function (e) {
+    const options = {
+      body: "Contenido de la notificación push simulada.",
+      //icon: "icons/icon-192x192.png",
+      //badge: "icons/badge.png",
+      data: {
+        url: "https://ejemplo.com/notificacion",
+      },
+    };
+    d=e;
+    console.log(e);
+    e.showNotification("Notificación Push Simulada", options);
+  });
+}
