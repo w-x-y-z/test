a1c5ba7eeb541a7d16728094201596a7578b974b
w-x-y-z <146482222+w-x-y-z@users.noreply.github.com>
2024-05-06 18:54:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let d;
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("https://w-x-y-z.github.io/test/sw.js")
@@ -9,23 +9,6 @@ if ("serviceWorker" in navigator) {
       console.log("Error al registrar el Service Worker:", error);
     });
 }
-
-function simularNotificacionPush() {
-  navigator.serviceWorker.ready.then(function (e) {
-    const options = {
-      body: "Contenido de la notificación push simulada.",
-      //icon: "icons/icon-192x192.png",
-      //badge: "icons/badge.png",
-      data: {
-        url: "https://ejemplo.com/notificacion",
-      },
-    };
-    d=e;
-    console.log(e);
-    e.showNotification("Notificación Push Simulada", options);
-  });
-}
-
 // Agregar evento de clic al botón
 //document.getElementById('notifyButton').addEventListener('click', showNotification);
 notifyButton.addEventListener("click", () => {
