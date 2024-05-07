9879ba0628591a39ae19ac265380551bd9069b09
w-x-y-z <146482222+w-x-y-z@users.noreply.github.com>
2024-05-06 19:41:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
+///register("https://w-x-y-z.github.io/test/sw.js")
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("https://w-x-y-z.github.io/test/sw.js")
     .then(function (registration) {
-      console.log("Service Worker registrado correctamente");
+      console.log("Service Worker registrado correctamente",registration);
+      sw=registration;
     })
     .catch(function (error) {
       console.log("Error al registrar el Service Worker:", error);
     });
 }
+
+
 // Agregar evento de clic al botón
 //document.getElementById('notifyButton').addEventListener('click', showNotification);
 notifyButton.addEventListener("click", () => {
   debugger;
-  simularNotificacionPush();
+  tesNotificacionPush();
 });
+
+
+// Crear un nuevo evento personalizado
+var pushEvent = new CustomEvent('push', {
+  detail: {
+    message: 'Nuevo mensaje push recibido',
+    data: {
+      // Aquí puedes incluir cualquier dato adicional que desees enviar con el evento push
+    }
+  }
+});
+// Despachar el evento en el objeto global (window)
+//window.dispatchEvent(pushEvent);
